4d4dbdabd499db608659d799e31ec737c876cf38
slaughtermc <47281879+slaughtermc@users.noreply.github.com>
2020-05-03 16:18:10 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -160,11 +160,7 @@ PRODUCT_BOOT_JARS += \
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    lineage.trust@1.0-service
-
+    
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk-sp
